80cb6adcb365bd27ddfdc7915e3171f2eefd1279
allomhg <allomhg@gmail.com>
2023-06-03 13:44:01 +0100

    Functional code. Passes all tests.

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,12 +1,25 @@
 function rot13(str) {
-    // get the unicode of each character, +13, ???, profit
-    // perfect plan !!!!
-    var myArray = str.split('');
-    console.log(myArray);
-    //console.log(str.fromCharCode())
-    myArray.forEach(function (input) {
-        //console.log(input.fromCharCode())
-    });
-    return str;
+    var decipherStr = '';
+    for (var i in str) {
+        var codePoint = str.codePointAt(i);
+        console.log(codePoint - 65);
+        if (codePoint == 32) {
+            decipherStr += ' ';
+        }
+        else if ((codePoint - 13 < 65) && (codePoint - 65 >= 0)) {
+            decipherStr += String.fromCharCode(65 + 13 + (codePoint - 65));
+        }
+        else if (codePoint - 13 >= 65) {
+            decipherStr += String.fromCharCode(codePoint - 13);
+        }
+        else {
+            decipherStr += String.fromCharCode(codePoint);
+        }
+    }
+    console.log(decipherStr);
+    return decipherStr;
 }
 rot13("SERR PBQR PNZC");
+rot13("SERR CVMMN!");
+rot13("SERR YBIR?");
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
--- caesarsCipher.ts
+++ caesarsCipher.ts
@@ -1,17 +1,28 @@
-function rot13(str) {
-  // get the unicode of each character, +13, ???, profit
-  // perfect plan !!!!
+function rot13(str) : string {
+  let decipherStr = '';
 
-  let myArray = str.split('');
-  console.log(myArray);
-  //console.log(str.fromCharCode())
+  for (let i in str) {
+    const codePoint = str.codePointAt(i);
+    console.log(codePoint - 65);
 
-  myArray.forEach( input => {
-    
-    //console.log(input.fromCharCode())
-    });
 
-  return str;
+    if ( codePoint == 32 ) {
+      decipherStr += ' ';
+    } else if ( (codePoint - 13 < 65) && ( codePoint - 65 >= 0)) {
+      decipherStr += String.fromCharCode( 65 + 13 + ( codePoint - 65 ));
+    } else if ( codePoint - 13 >= 65 ) {
+      decipherStr += String.fromCharCode(codePoint - 13);
+    } else {
+      decipherStr += String.fromCharCode(codePoint);
+    }
+  }
+
+  console.log(decipherStr);
+
+  return decipherStr;
 }
 
 rot13("SERR PBQR PNZC");
+rot13("SERR CVMMN!");
+rot13("SERR YBIR?");
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
